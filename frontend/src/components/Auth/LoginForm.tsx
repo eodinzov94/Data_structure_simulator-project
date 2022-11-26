b8e054e830da23025b768acefe2eb461896829fc
gilbenhamo <gilbh859@gmail.com>
2022-11-26 23:43:02 +0200

    added dynamic color for loginPage

--- frontend/src/components/Auth/LoginForm.tsx
+++ frontend/src/components/Auth/LoginForm.tsx
@@ -1,8 +1,9 @@
-import { LockClosedIcon } from '@heroicons/react/20/solid'
+import { LockClosedIcon } from "@heroicons/react/20/solid";
+import { mainColor, mainHoverColor } from "../../styles/tColors";
 
 const LoginForm = () => {
-    return (
-        <form className="mt-8 space-y-6" action="#" method="POST">
+  return (
+      <form className="mt-8 space-y-6" action="#" method="POST">
         <input type="hidden" name="remember" defaultValue="true" />
         <div className="-space-y-px rounded-md shadow-sm">
           <div>
@@ -15,7 +16,7 @@ const LoginForm = () => {
               type="email"
               autoComplete="email"
               required
-              className="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              className={`relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-${mainColor} focus:outline-none focus:ring-${mainColor} sm:text-sm`}
               placeholder="Email address"
             />
           </div>
@@ -29,7 +30,7 @@ const LoginForm = () => {
               type="password"
               autoComplete="current-password"
               required
-              className="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+              className={`relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-${mainColor} focus:outline-none focus:ring-${mainColor} sm:text-sm`}
               placeholder="Password"
             />
           </div>
@@ -39,7 +40,7 @@ const LoginForm = () => {
           <div className="text-sm">
             <a
               href="#"
-              className="font-medium text-indigo-600 hover:text-indigo-500"
+              className={`font-medium text-${mainHoverColor} hover:text-gray-900`}
             >
               Forgot your password?
             </a>
@@ -53,7 +54,7 @@ const LoginForm = () => {
           >
             <span className="absolute inset-y-0 left-0 flex items-center pl-3">
               <LockClosedIcon
-                className="h-5 w-5 text-lime-600 group-hover:text-lime-500"
+                className={`h-5 w-5 text-${mainHoverColor} group-hover:text-${mainColor}`}
                 aria-hidden="true"
               />
             </span>
@@ -61,7 +62,7 @@ const LoginForm = () => {
           </button>
         </div>
       </form>
-    );
+  );
 };
 
 export default LoginForm;
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,4 +1,5 @@
 import LoginForm from "../components/Auth/LoginForm";
+import { mainColor, mainHoverColor } from "../styles/tColors";
 
 const LoginPage = () => {
   return (
@@ -11,27 +12,30 @@ const LoginPage = () => {
         <body class="h-full">
         ```
       */}
-      <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-        <div className="w-full max-w-md space-y-8">
-          <div>
-            <img
-              className="mx-auto h-12 w-auto"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
-            />
-            <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-            <p className="mt-2 text-center text-sm text-gray-600">
-              Or{" "}
-              <a
-                href="#"
-                className="font-medium text-indigo-600 hover:text-indigo-500"
-              >
-                Sign up now!
-              </a>
-            </p>
-            <LoginForm/>
+      {/* pt-64 for padding top */}
+      <div className="flex min-h-full items-center justify-center py-12 px-2 sm:px-4 lg:px-8 ">
+        <div className="max-w-sm w-full p-10 bg-white border border-gray-200 rounded-lg shadow-lg">
+          <div className="w-full max-w-md space-y-8 ">
+            <div>
+              <img
+                className="mx-auto h-12 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=lime&shade=500"
+                alt="Your Company"
+              />
+              <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+                Sign in to your account
+              </h2>
+              <p className="mt-2 text-center text-sm text-gray-600">
+                Or{" "}
+                <a
+                  href="#"
+                  className={`font-medium text-${mainColor} hover:text-${mainHoverColor}`}
+                >
+                  Sign up now!
+                </a>
+              </p>
+              <LoginForm />
+            </div>
           </div>
         </div>
       </div>
@@ -40,4 +44,3 @@ const LoginPage = () => {
 };
 
 export default LoginPage;
-
