0333696543b335d91d29ea0f9de7fc014429df3a
yovelal <aloniyovel1020@gmail.com>
2022-11-28 22:15:43 +0200

    design home page

---
+++ frontend/src/components/Home/HomePageData.ts
@@ -0,0 +1,78 @@
+const HomePageData= [
+    {
+        title: "Stack",
+        image: "https://bit.ly/3BQdTqk",
+        url: "/login",
+      },
+      {
+        title: "Queue",
+        image: "https://bit.ly/3BQdTqk",
+        url: "/login",
+      },
+      {
+        title: "Quick sort",
+        image: "https://bit.ly/3BQdTqk",
+        url: "/login",
+      },
+      {
+        title: "Insertion sort",
+        image: "https://bit.ly/3BQdTqk",
+        url: "/login",
+      },
+      {
+        title: "Counting sort",
+        image: "https://bit.ly/3BQdTqk",
+        url: "/login",
+      },
+    {
+      title: "Merge sort",
+      image: "https://bit.ly/3BQdTqk",
+      url: "/login",
+    },
+    {
+        title: "Radix sort",
+        image: "https://bit.ly/3BQdTqk",
+        url: "/login",
+      },
+      {
+        title: "Bucket sort",
+        image: "https://bit.ly/3BQdTqk",
+        url: "/login",
+      },
+      {
+        title: "Linked List",
+        image: "https://bit.ly/3CQKSwb",
+        url: "/login",
+  
+      },
+      {
+        title: "Hash table",
+        image: "https://bit.ly/3BQdTqk",
+        url: "/login",
+      },
+      {
+        title: "Heap",
+        image: "https://bit.ly/3BQdTqk",
+        url: "/login",
+      },
+
+      {
+        title: "Binary tree",
+        image: "https://bit.ly/3BQdTqk",
+        url: "/login",
+      },
+    {
+      title: "Binary search tree",
+      image: "https://bit.ly/3CQFPvv",
+      url: "/login",
+
+    },
+    {
+      title: "AVL Tree",
+      image: "https://bit.ly/3ERuyMd",
+      url: "/login",
+
+    }
+  ];
+
+  export default HomePageData;
