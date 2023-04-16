3e67acd523b0e1565a96ab41493a9bbaccafda4e
yovel aloni <aloniyovel1020@gmail.com>
2023-04-16 18:29:18 +0300

    add export buttons to the algorithms reports page

--- frontend/src/pages/general/AlgorithmsReportsPage.tsx
+++ frontend/src/pages/general/AlgorithmsReportsPage.tsx
@@ -1,10 +1,7 @@
 import BarChart from "../../components/Charts/BarChart";
 import FloatUpContainer from "../../components/UI/FloatUpContainer";
 import MediumCard from "../../components/UI/MediumCard";
-import {
-  getData,
-  getLabels,
-} from "../../components/Charts/interface";
+import { getData, getLabels } from "../../components/Charts/interface";
 import { useState } from "react";
 import { Bar } from "react-chartjs-2";
 import {
@@ -12,6 +9,8 @@ import {
   DUMMY,
   DUMMY1,
 } from "../../components/Charts/charts-utils";
+import ExportExcel from "../../components/Charts/ExportExcel";
+import { makeTable } from "../../utils/helper-functions";
 
 const AlgorithmsReportPage = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
@@ -28,6 +27,13 @@ const AlgorithmsReportPage = () => {
           title={"Alogrithms usage details"}
           onClick={barClickHandler}
         />
+        <ExportExcel
+          fileName={'algorithms-reports'}
+          csvData={makeTable({
+            items: DUMMY,
+            title:'algorithms-reports',
+          })}
+        />
 
         {selectedIndex > 0 && DUMMY1[selectedIndex].length > 0 && (
           <>
@@ -38,9 +44,20 @@ const AlgorithmsReportPage = () => {
               data={{
                 labels: getLabels(DUMMY1[selectedIndex]),
                 datasets: [
-                  { label:`${DUMMY[selectedIndex].key} details`,data: getData(DUMMY1[selectedIndex]), ...datasetConfig },
+                  {
+                    label: `${DUMMY[selectedIndex].key} details`,
+                    data: getData(DUMMY1[selectedIndex]),
+                    ...datasetConfig,
+                  },
                 ],
               }}
+            />
+            <ExportExcel
+              fileName={`${DUMMY[selectedIndex].key}-reports`}
+              csvData={makeTable({
+                items: DUMMY1[selectedIndex],
+                title: DUMMY[selectedIndex].key,
+              })}
             />
           </>
         )}
