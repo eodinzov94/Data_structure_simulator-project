719fc37bd4e0d24c6079755f87eac5c83962a5c2
yovel aloni <aloniyovel1020@gmail.com>
2023-04-30 14:19:34 +0300

    edit profile page

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -2,9 +2,10 @@ import AuthCard from "../components/Auth/AuthCard";
 import FormButton from "../components/Auth/FormButton";
 import FloatUpContainer from "../components/UI/FloatUpContainer";
 import { useAppSelector } from "../store/hooks";
-import { LockOpenIcon, PencilIcon } from '@heroicons/react/20/solid'
+import { LockOpenIcon, PencilIcon } from "@heroicons/react/20/solid";
 import { ProfileLime500 } from "../utils/logos";
-
+import RoutePaths from "../Routes/RoutePaths";
+import { Link } from "react-router-dom";
 
 const ProfilePage = () => {
   const user = useAppSelector((state) => state.auth.user!);
@@ -12,69 +13,68 @@ const ProfilePage = () => {
   return (
     <>
       <FloatUpContainer>
-      <AuthCard title={"Profile"}>
-
-        <div className="bg-white p-3 shadow-sm rounded-sm">
-          <div className="text-gray-700 rows-1 break-after-column">
-            <div className="grid md:grid-cols-1 text-sm">
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">First Name</div>
-                <div className="px-4 py-2">{user.firstName}</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Last Name</div>
-                <div className="px-4 py-2">{user.lastName}</div>
-              </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Email</div>
-                <div className="px-4 py-2">
-                  <a className="text-blue-800" href="mailto:jane@example.com">
-                  {user.email}
-                  </a>
+        <AuthCard title={"Profile"}>
+          <div className="bg-white p-3 shadow-sm rounded-sm">
+            <div className="text-gray-700 rows-1 break-after-column">
+              <div className="grid md:grid-cols-1 text-sm">
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">First Name</div>
+                  <div className="px-4 py-2">{user.firstName}</div>
                 </div>
-              </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Last Name</div>
+                  <div className="px-4 py-2">{user.lastName}</div>
+                </div>
+                <div className="grid grid-cols-2">
+                  <div className="px-4 py-2 font-semibold">Email</div>
+                  <div className="px-4 py-2">
+                    <a className="text-blue-800" href="mailto:jane@example.com">
+                      {user.email}
+                    </a>
+                  </div>
+                </div>
 
-            {user.role !== 'Lecturer' &&
-            <>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Birthyear</div>
-                <div className="px-4 py-2">{user.birthYear}</div>
+                {user.role !== "Lecturer" && (
+                  <>
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">Birthyear</div>
+                      <div className="px-4 py-2">{user.birthYear}</div>
+                    </div>
+                    <div className="grid grid-cols-2">
+                      <div className="px-4 py-2 font-semibold">Gender</div>
+                      <div className="px-4 py-2">{user.gender}</div>
+                    </div>
+                  </>
+                )}
               </div>
-              <div className="grid grid-cols-2">
-                <div className="px-4 py-2 font-semibold">Gender</div>
-                <div className="px-4 py-2">{user.gender}</div>
+              <div className="space-y-3 w-80">
+                <Link to={RoutePaths.EDIT_PROFILE}>
+                  <FormButton
+                    type={"button"}
+                    title={"Edit profile"}
+                    onClick={() => {}}
+                    icon={
+                      <PencilIcon
+                        className={`h-5 w-5 text-lime-600 group-hover:text-lime-500`}
+                        aria-hidden="true"
+                      />
+                    }
+                  />
+                </Link>
+
+                <FormButton
+                  type={"button"}
+                  title={"Change password"}
+                  icon={
+                    <LockOpenIcon
+                      className={`h-5 w-5 text-lime-600 group-hover:text-lime-500`}
+                      aria-hidden="true"
+                    />
+                  }
+                />
               </div>
-            </>
-            }
-
-
-          </div>
-          <div className="space-y-3 w-80">
-          <FormButton
-            type={"button"}
-            title={"Edit profile"}
-            onClick={()=>{}}
-            icon={
-              <PencilIcon
-                className={`h-5 w-5 text-lime-600 group-hover:text-lime-500`}
-                aria-hidden="true"
-              />
-            }
-          />
-          <FormButton
-            type={"button"}
-            title={"Change password"}
-            icon={
-              <LockOpenIcon
-                className={`h-5 w-5 text-lime-600 group-hover:text-lime-500`}
-                aria-hidden="true"
-              />
-            }/>
             </div>
-        </div>
-        </div>
-
-
+          </div>
         </AuthCard>
       </FloatUpContainer>
       <FloatUpContainer>
