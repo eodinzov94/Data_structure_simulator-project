cc2319ac73c8d14edc64dcf088d642c6af57948d
evgeny <eodinzov94@gmail.com>
2023-05-21 02:00:40 +0300

    test branch color fill

--- frontend/src/components/Simulation/BinaryTree/Branch.tsx
+++ frontend/src/components/Simulation/BinaryTree/Branch.tsx
@@ -11,3 +11,20 @@ const Branch: FC<BranchProps> = ({branch}) => {
 }
 export default Branch
 
+
+/* test
+const Branch: FC<BranchProps> = ({branch}) => {
+  return (<motion.span
+      className='branch'
+      exit={{opacity:0}}
+      style={{...branch.getStyle(),background: "linear-gradient(to right, black, red)",
+        backgroundSize: "200% 100%",
+        backgroundPosition: "100% 0%",}}
+      animate={{ backgroundPosition: "-100% 0%" }}
+      transition={{
+        duration: 1,
+        ease: "linear",
+      }}
+     />)
+}
+* */
